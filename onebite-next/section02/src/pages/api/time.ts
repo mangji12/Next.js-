b2d5d814ce9767bfa06de45149ff080ab05960bd
dhckddls12 <dhckddls12@gmail.com>
2026-01-07 11:33:33 +0000

    예외처리 파일 수정

--- onebite-next/section02/src/pages/api/time.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest, NextResponse } from "next/server"
-
-export default function handler(
-    req: NextRequest,
-    res: NextResponse
-) {
-    const date = new Date();
-    res.json({time: date.toLocaleString() });
-}
