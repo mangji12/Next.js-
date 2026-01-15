340a76ee3b503aa147c8bf262db308e6a4c65c7d
dhckddls12 <dhckddls12@gmail.com>
2026-01-15 14:06:30 +0000

    fix: 서치바 클라이언트 컴포넌트로 변경

--- onebite-next/section03/src/app/(with-searchbar)/layout.tsx
+++ onebite-next/section03/src/app/(with-searchbar)/layout.tsx
@@ -1,10 +1,12 @@
 import { ReactNode } from "react";
+import Searchbar from "./searchbar";
 
 export default function Layout({children} : Readonly<{children: ReactNode}>) {
     return (
         <div>
             <div>임시 서치바</div>
             {children}
+            <Searchbar/>
         </div>
     )
 }
