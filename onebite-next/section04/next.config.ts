add17eddc2928b87476a41b96183c109b0a3e955
dhckddls12 <dhckddls12@gmail.com>
2026-01-16 14:59:09 +0000

    fix: 모크 데이터를 실제 데이터로 바꿈

---
+++ onebite-next/section04/next.config.ts
@@ -0,0 +1,13 @@
+// next.config.ts
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  experimental: {
+    // @ts-ignore: Next.js 15의 특정 패치 버전에서 타입 미지원 대응
+    allowedDevOrigins: [
+      "3000-firebase-my-nextjs-app-1765356299225.cluster-isls3qj2gbd5qs4jkjqvhahfv6.cloudworkstations.dev"
+    ]
+  } as any // 타입을 강제로 지정해서 실행 시점에 옵션이 들어가게 함
+};
+
+export default nextConfig;
---
+++ onebite-next/section04/src/app/(with-searchbar)/search/page.tsx
@@ -0,0 +1,21 @@
+import BookItem from "@/components/book-item";
+import { BookData } from "@/types";
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{ q?: string }>;
+}) {
+  const { q } = await searchParams;
+
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`);
+  const books : BookData[] = await response.json();
+
+  return (
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+}
